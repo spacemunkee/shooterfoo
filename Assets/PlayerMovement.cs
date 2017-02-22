fc83cc8ed98bd89197337ccdaadfcf852b114c52
Doug Linley <spacemunkee.codes@gmail.com>
2017-02-21 21:01:44 -0500

    Added rotation to the sprite via the horizontal axis

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	public float playerSpeed = 5f;
+	public float rotationSpeed = 180f;
 
 	// Update is called once per frame
 	void Update () {
+		// rotation
+		Quaternion rotation = transform.rotation;
+		float z = rotation.eulerAngles.z;
+		z += Input.GetAxis ("Horizontal") * rotationSpeed * Time.deltaTime;
+		rotation = Quaternion.Euler (0, 0, z);
+		transform.rotation = rotation;
+
+		// vertical movement
 		Vector3 position = transform.position;
 		position.y += Input.GetAxis ("Vertical") * playerSpeed * Time.deltaTime;
 		transform.position = position;
