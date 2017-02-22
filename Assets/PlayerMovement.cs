454f0bf2f9008d9fcfba7ae2c2ee3aceb44b9616
Doug Linley <spacemunkee.codes@gmail.com>
2017-02-21 20:53:38 -0500

    Added Vertical Player Movement
    The player is now able to move up and down. I also organized some of the assets in the project.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float playerSpeed = 5f;
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = transform.position;
+		position.y += Input.GetAxis ("Vertical") * playerSpeed * Time.deltaTime;
+		transform.position = position;
+	}
+}
