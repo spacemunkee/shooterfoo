456cb6fad54804143da14eeb3540788cf53dcaad
Doug Linley <spacemunkee.codes@gmail.com>
2017-02-21 21:05:59 -0500

    Refactored Rotation
    Refactored movement and rotation into their own methods. Also fixed a big where the rotation direction was incorrect.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -9,16 +9,21 @@ public class PlayerMovement : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		// rotation
-		Quaternion rotation = transform.rotation;
-		float z = rotation.eulerAngles.z;
-		z += Input.GetAxis ("Horizontal") * rotationSpeed * Time.deltaTime;
-		rotation = Quaternion.Euler (0, 0, z);
-		transform.rotation = rotation;
+		RotationUpdate ();
+		VerticalMovementUpdate ();
+	}
 
-		// vertical movement
+	void VerticalMovementUpdate() {
 		Vector3 position = transform.position;
 		position.y += Input.GetAxis ("Vertical") * playerSpeed * Time.deltaTime;
 		transform.position = position;
+	}
+
+	void RotationUpdate() {
+		Quaternion rotation = transform.rotation;
+		float z = rotation.eulerAngles.z;
+		z -= Input.GetAxis ("Horizontal") * rotationSpeed * Time.deltaTime;
+		rotation = Quaternion.Euler (0, 0, z);
+		transform.rotation = rotation;
 	}
 }
