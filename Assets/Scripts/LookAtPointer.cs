dd754938463ddf887a0e0456871f14a4b34af621
Doug Linley <spacemunkee.codes@gmail.com>
2017-02-24 15:59:37 -0500

    Wave Spawner, Enemy Deletion, Ship Rotation
    The wave spawner is now in the game but there are a couple of bugs. The wave spawner will only spawn the first wave. It also complains that it can't find the enemy tag when there are no enemies available.
    
    Enemies are now destroyed if they travel off screen.
    
    The ship is now locked to the middle of the screen and rotates to look at the mouse pointer.

---
+++ Assets/Scripts/LookAtPointer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPointer : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 pointerPosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		transform.rotation = Quaternion.LookRotation (Vector3.forward, pointerPosition - transform.position);
+	}
+}
