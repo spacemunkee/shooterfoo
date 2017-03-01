d9ba180fb6e9f71bec6cb71c5d7fcc9588579927
Doug Linley <spacemunkee.codes@gmail.com>
2017-02-28 19:55:14 -0500

    Shooting & Bug Fixes
    Fixed bugs that were showing in the debug console.
    
    Added player shooting.

---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+
+	public GameObject shotPrefab;
+	public float shootRate = 0.25f;
+	float countdownTimer = 0f;
+
+	void Update () {
+		countdownTimer -= Time.deltaTime;
+
+		if (Input.GetMouseButton (0) && countdownTimer <= 0) {
+			countdownTimer = shootRate;
+			Debug.Log ("Pew");
+			Vector3 offset = transform.rotation * new Vector3 (0, 0.5f, 0);
+
+			Instantiate (shotPrefab, transform.position + offset, transform.rotation);
+		}
+	}
+}
---
+++ Assets/Scripts/shotMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shotMovement : MonoBehaviour {
+
+	public float speed = 5f;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = transform.position;
+
+		Vector3 velocity = new Vector3 (0, speed * Time.deltaTime, 0);
+
+		position += transform.rotation * velocity;
+
+		transform.position = position;
+	}
+}
